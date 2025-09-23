493bf7583ea34d73da0a0e80c2e4138a34455367
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-09-23 15:39:33 +0200

    add Irrigation pipeline skeleton

--- src/irrigation_processor/main.py
+++ src/irrigation_processor/main.py
@@ -1,17 +1,12 @@
 import json
 
-from src.irrigation_processor.pipeline import (
-    InlineService,
-    FileStorage,
-    Pipeline,
-)
-
+from src.irrigation_processor.pipeline import FileStorage, LocalService, Pipeline
 from src.irrigation_processor.steps import step
 
 registry = step.get_registry()
 
 storage = FileStorage("./pipeline_storage")
-service = InlineService(storage=storage)
+service = LocalService(storage=storage)
 p = Pipeline(service=service)
 
 p.add_steps_from_registry(registry)
--- src/irrigation_processor/steps.py
+++ src/irrigation_processor/steps.py
@@ -1,41 +1,77 @@
-from src.irrigation_processor.pipeline import FromTask, Step
-
 from pydantic import BaseModel
 
+from src.irrigation_processor.pipeline import FromTask, Step
 
 step = Step()
 
+
 class BaseContext(BaseModel):
     step_name: str
-    time_period: tuple = ("2020-01-01", "2020-01-31")
+    time_range: tuple = ("2020-01-01", "2020-01-31")
+    bbox: list = []
+    crs: str = ""
+
+
+class DataLoaderContext(BaseContext):
+    """"""
+
+
+class PreprocessorContext(BaseContext):
+    """"""
+
+
+class CalibratorContext(BaseContext):
+    """"""
+
+
+class SimulatorContext(BaseContext):
+    """"""
+
+
+@step.register(name="dataloader", context_cls=BaseContext)
+def dataloader(context: DataLoaderContext):
+    from src.irrigation_processor.dataloader import load_data
+
+    return load_data(context)
+
 
 @step.register(
-    inputs=(FromTask("preprocessing", "preprocessed"),),
-    outputs=("calibrated",),
-    name="calibration",
-    context_cls=BaseContext
-)
-def calibration(context, preprocessed):
-    print("inside calibration, got preprocessed:", preprocessed)
-    return {"calibrated": {"data": [x * 2 for x in preprocessed["data"]]}}
-
-
-@step.register(
-    outputs=("preprocessed",),
+    inputs=(
+        FromTask("dataloader", "sm_path"),
+        FromTask("dataloader", "lc_path"),
+        FromTask("dataloader", "era5_path"),
+    ),
     name="preprocessing",
-    context_cls=BaseContext
+    context_cls=PreprocessorContext,
 )
-def preprocessing(context: BaseContext):
-    print("inside preprocessing, context:", context.time_period)
-    return {"preprocessed": {"data": [1, 2, 3]}, "int_val": 44, "str_val": "meow"}
+def preprocessing(
+    context: PreprocessorContext, sm_path: str, lc_path: str, era5_path: str
+):
+    from src.irrigation_processor.preprocessor import irrigation_preprocessor
+
+    return irrigation_preprocessor(context, sm_path, lc_path, era5_path)
+
 
 @step.register(
-    inputs=(FromTask("calibration", "calibrated"),),
-    outputs=("model",),
-    name="training",
-    context_cls=BaseContext
+    inputs=(FromTask("preprocessing", "preprocessed_path"),),
+    name="calibration",
+    context_cls=CalibratorContext,
 )
-def training(context, calibrated):
-    print("inside training, got calibrated:", calibrated)
-    return {"model": {"coef": sum(calibrated["data"])}}
+def calibration(context: CalibratorContext, preprocessed_path: str):
+    from src.irrigation_processor.calibrator import soil_moisture_inversion_calibration
 
+    return soil_moisture_inversion_calibration(context, preprocessed_path)
+
+
+@step.register(
+    inputs=(
+        FromTask("preprocessing", "preprocessed_path"),
+        FromTask("calibration", "calibrated_path"),
+    ),
+    name="simulation",
+    context_cls=SimulatorContext,
+)
+def simulation(context: SimulatorContext, preprocessed_path: str, calibrated_path: str):
+    from src.irrigation_processor.simulator import irrigation_simulator
+
+    return irrigation_simulator(context, preprocessed_path, calibrated_path)
